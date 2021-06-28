ca413d5c3d32ecd60b2974a1dcab044c15b9f5ea
HARIPRASAD S A <myharicode@gmail.com>
2021-06-28 14:55:20 +0530

    sin-project

--- CELF/inffind.py
+++
@@ -1,6 +0,0 @@
-def inffind(graph, novert, seed_size, outdeg, model): 
-    if model == "IC":
-        seeds = propic(graph, novert, seed_size, outdeg) 
-    else:
-        seeds = proplt(graph, novert, seed_size, outdeg) 
-    return seeds
